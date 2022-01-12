eb340d32bbdf68e9ad037fda7d366dd930f8e496
shubhamshewale96 <shubhamshewale1996@gmail.com>
2022-01-12 23:41:47 +0530

    write test case

--- src/main/java/com/employeetest/model/Employee.java
+++ src/main/java/com/employeetest/model/Employee.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import lombok.Builder;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
 import java.sql.Date;
@@ -12,7 +14,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
-@Data
+
 @Table(name = "employeeTest")
 public class Employee {
     @Id
@@ -25,5 +27,68 @@ public class Employee {
     @JsonDeserialize(using = LocalDateDeserializer.class)
     @JsonSerialize(using = LocalDateSerializer.class)
     private LocalDateTime startDate;
+    private String department;
+    private String note;
 
+
+    public Employee() {
+    }
+
+    public Employee(int id, String name, long salary, String gender, LocalDateTime startDate, String department, String note) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.gender = gender;
+        this.startDate = startDate;
+        this.department = department;
+        this.note = note;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
 }
--- src/test/java/com/employeetest/demo/DemoApplicationTests.java
+++ src/test/java/com/employeetest/demo/DemoApplicationTests.java
@@ -1,13 +1,74 @@
 package com.employeetest.demo;
 
+import com.employeetest.model.Employee;
+import com.employeetest.repository.Repository;
+import com.employeetest.service.Service;
+
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
+
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
 
 @SpringBootTest
 class DemoApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Mock
+    private Repository repository;
+
+    @InjectMocks
+    private Service service;
+
+    private static Employee emp1;
+    private static Employee emp2;
+    private static List<Employee> e1 =null;
+
+    @BeforeAll
+    public static void init() {
+
+        // create an Employee Entity object
+        emp1 = new Employee(1, "Vinod", 40000, "male", LocalDateTime.parse("2018-12-30T19:34:50.63"),"Hr","abcd");
+        emp2 = new Employee(2, "Shubham", 20000, "male", LocalDateTime.parse("2018-12-30T19:34:50.63"),"account","abcd");
+
+        e1=new ArrayList<>();
+        e1.add(emp1);
+        e1.add(emp2);
+
     }
+    //get all list employee
+    @Test
+    public void getAllTest() {
+
+        Mockito.when(repository.findAll()).thenReturn(e1);
+        List allEmployees = service.getAllEmp();
+
+        assertNotNull(allEmployees);
+        assertEquals(2, allEmployees.size());
+    }
+
+    //add employee
+    @Test
+    public void addEmployeeTest() {
+        Mockito.when(repository.save(emp1)).thenReturn(emp1);
+        Employee emp = service.save(emp1);
+        assertNotNull(emp);
+        assertEquals(emp1,emp);
+    }
+
 
 }
